226018aed8d35908e25b4501918b2060330b85e1
remah massri <remah.massri@gmail.com>
2020-06-10 15:22:31 +0300

    refactroing lab

---  js/app.js
+++  js/app.js
@@ -1,77 +1,58 @@
 'use strict';
-
-var correctAnswerCounter = 0;
+var correctAnswer = 0;
 var userName;
 function guessName(){
-    userName = prompt(' enter your name');
-    alert('Hello ' + userName + ' , welcome to my personal website');
-    alert('Please try to guess the following five questions by answering y/yes or n/no')
+    userName = prompt('please tell me  your name');
+    alert('Hi ' + userName + ' , welcome to my small guessing game!');
+    alert(' play with me  to guess the following five questions by answering y/yes or n/no')
 }
 
 function guessAge(){
-    var Age = prompt('I am in my 30s, yes or no ?');
-
+    var Age = prompt('I am in my 20s, yes or no ?');
     if(Age.toLowerCase() == 'yes' || Age.toLowerCase() =='y'){
-        aleAgert('oops, wrong answer! I am in my 20s');
-        console.log('I am in my 30s, yes or no ?');
-        console.log('you answered yes, but I am not, I am in my 20s');
-        correctAnswerCounter++ ;
-
+        aleAgert('NO, wrong answer! I am in my 20s');
+        correctAnswer++ ;
     }else if (Age.toLowerCase() == 'no' || Age.toLowerCase() =='n'){
         alert('yes, that is correct! I am in my 20s');
-        console.log('I am in my 30s, yes or no ?');
-        console.log('yes that is correct');
     }// end if 1
 }
 
 function guessStudies(){
-    
-    var Studies = prompt('Am I an engineer?');
+    var Studies = prompt('Am I an software engineer?');
 
     if(Studies.toLowerCase() == 'yes' || Studies.toLowerCase() == 'y'){
         alert('yes, that is correct answer :)');
-        console.log('Am I an engineer?');
-        console.log('you answered yes and that is correct')
-        correctAnswerCounter ++ ;
+        correctAnswer ++ ;
 
     } else if (Studies.toLowerCase() == 'no' || Studies.toLowerCase() =='n'){
-        alert('oops, wrong answer! I am an engineer')
-        console.log('Am I an engineer?');
-        console.log('you answered No, but I am an engineer');
+        alert('no, wrong answer! I am an engineer')
     }// end if 2
 }
 
 function guessWork(){
 
-    var lastWork = prompt('Did I work in e-commerce bussines?');
+    var lastWork = prompt('Did I work in health-products?');
 
     if(lastWork.toLowerCase() == 'yes' || lastWork.toLowerCase() == 'y'){
         alert('yes, that is correct :)')
-        console.log('Did I work in e-commerce bussines?')
-        console.log('you answered yes and that is correct')
-        correctAnswerCounter++ ;
+        correctAnswer++ ;
     
     }else if (lastWork.toLowerCase() == 'no' || lastWork.toLowerCase() == 'n'){
-        alert('oops, wrong answer! I did')
-        console.log('Did I work in e-commerce bussines?')
-        console.log('you answered No, but I did');
+        alert('NO, wrong answer! I did')
+       
     }// end if 3
 }
 
 function guessOther(){
 
-    var OtherlastWork = prompt('Do I know about photographing?');
+    var OtherlastWork = prompt('Do I know about makeup?');
 
     if(OtherlastWork.toLowerCase() == 'yes' || OtherlastWork.toLowerCase() == 'y'){
         alert('yes, that is correct :)')
-        console.log('Do I know about photographing?')
-        console.log('you answered yes and that is correct')
-        correctAnswerCounter++ ;
+        correctAnswer++ ;
     
     }else if (OtherlastWork.toLowerCase() == 'no' || OtherlastWork.toLowerCase() =='n'){
-        alert('oops, wrong answer! I do')
-        console.log('Do I know about photographing?')
-        console.log('you answered No, but I do');
+        alert('NO, wrong answer! I did')
     }// end if 4
 }
 
@@ -80,15 +61,11 @@ function guessYear(){
     var programmingStartingYear = prompt('Did I start studying programming in 2019?');
 
     if(programmingStartingYear.toLowerCase() == 'yes' || programmingStartingYear.toLowerCase() == 'y'){
-        alert('oops, wrong answer! I started in 2020');
-        console.log('Did I start studying programming in 2019?');
-        console.log('you answered yes, but I did');
-        correctAnswerCounter++ ;
+        alert('NO, wrong answer! I started in 2017');
+        correctAnswer++ ;
     
     }else if (programmingStartingYear.toLowerCase() == 'no' || programmingStartingYear.toLowerCase() == 'n'){
         alert('yes, that is correct :)')
-        console.log('Did I start studying programming in 2019?');
-        console.log('you answered no and that is correct')
     }// end if 5
 }
 
@@ -101,17 +78,16 @@ function guessNum(){
         alert('it`s too low')
     }else {
         alert('that is correct');
-        correctAnswerCounter++ ;
+        correctAnswer++ ;
     }
     }// end for 6
         
-    console.log('The right number is 7');
     alert('The right number is 7');
 }
 
 function guessFood(){
     var x = 0;
-    var foodArray = ['pizza', 'burger', ' pasta', 'mlukieh '];
+    var foodArray = ['pizza', 'burger', ' pasta', 'maftool '];
     var foodChoice = prompt('Can you guess one of my favourite food? , you have six attempts');
     while ( x < 5 && foodChoice != foodArray[0] && foodChoice != foodArray[1] && foodChoice != foodArray[2] && foodChoice != foodArray[3]) {
         foodChoice = prompt('Can you guess one of my favourite food? , you have six attempts');
@@ -120,7 +96,7 @@ function guessFood(){
 
     if (x<5){
         alert('That is correct, nice guessing');
-        correctAnswerCounter++ ;
+        correctAnswer++ ;
         alert('Here is the list of my favourite food ' + foodArray)
     }else {
         alert('You run out of attempets and here is the list of my favourite food'+ foodArray)
@@ -129,9 +105,8 @@ function guessFood(){
 
 function score(){
     
-    alert('Thank you ' + userName + ' ,you have tried your best');
-    alert('The number of your correct answers is : ' + correctAnswerCounter);
-    console.log('The number of your correct answers is : ' + correctAnswerCounter);
+    alert('Thank you ' + userName + ' ,you are great ');
+    alert('your answer is  : ' + correctAnswer);
 }
 
 
